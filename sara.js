5c35c67fc6aced7ced11dcc95e35b3dabb82f777
Julia Kim <juliakimseohyeon@gmail.com>
2024-04-24 16:43:43 -0400

    add Keanu function to main javascript script

--- sara.js
+++ sara.js
@@ -45,10 +45,23 @@ async function showCocktail() {
 
 showCocktail();
 
+/* -------------------------------------------------------------------------- */
+/*                             get Keanu function                             */
+/* -------------------------------------------------------------------------- */
+const backgroundEl = document.querySelector("body");
+
+function generateRandomKeanu() {
+  const randNum = Math.floor(Math.random() * 100) + 450;
+  return randNum;
+}
+
 async function getKeanu() {
   try {
-    const response = await axios.get("https://placekeanu.com/600/400/");
-    console.log(response);
+    const response = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://placekeanu.com/${generateRandomKeanu()}/${generateRandomKeanu()}`
+    );
+    const keanuImgUrl = response.config.url;
+    backgroundEl.style.backgroundImage = `url('${keanuImgUrl}')`;
   } catch (error) {
     console.error(error);
   }
