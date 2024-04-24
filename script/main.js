3fbcc93f34d94e6da1543d9ce383f67f4a178b6b
Sara Chang <sara@Avocado.lan>
2024-04-24 15:38:04 -0700

    hide recipe on page load

--- script/main.js
+++ script/main.js
@@ -2,21 +2,21 @@ import keanu from "./keanu.js";
 import getCocktail from "./getCocktail.js";
 import showCocktail from "./showCocktail.js";
 
-getCocktail();
-showCocktail();
 keanu();
 
-const btnElement = document.querySelector('.btn');
+const btnElement = document.querySelector(".btn");
+const cocktailDiv = document.querySelector(".cocktail");
 
-btnElement.addEventListener('click',() => {
-    getCocktail();
-    showCocktail();
-    keanu();
-    playAudio();
-    btnElement.innerText = "HAVE ANOTHER DRINK WITH KEANU";
+btnElement.addEventListener("click", () => {
+  getCocktail();
+  showCocktail();
+  keanu();
+  playAudio();
+  btnElement.innerText = "HAVE ANOTHER DRINK WITH KEANU";
+  cocktailDiv.classList.remove("display-none");
 });
 
 function playAudio() {
-    var audio = new Audio('../audio/YES.wav');
-    audio.play();
+  var audio = new Audio("../audio/YES.wav");
+  audio.play();
 }
